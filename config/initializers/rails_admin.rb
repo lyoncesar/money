c227a77e34da38533ed1c1b01da866c61cbc569b
Lyon Oliveira <lyonoliveira@CX-TECH-0162.local>
2021-09-07 21:20:55 -0300

    Feat(I18n): Update pt-BR file

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,7 +3,7 @@ require 'i18n'
 RailsAdmin.config do |config|
   config.main_app_name = %w[Money]
 
-  # Model configurations
+  ## Model configurations
   config.label_methods << :description
   config.included_models = %w[FinancialControl User]
 
