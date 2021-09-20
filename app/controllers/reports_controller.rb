cb571910cbb7db21c780ad42f85d70189f64bf73
Lyon Oliveira <lyonoliveira@CX-TECH-0162.local>
2021-09-20 01:30:24 -0300

    Feat(Report): Update report with FinancialMovement
    Update the index report with FinancialMovement

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,10 @@
 class ReportsController < ApplicationController
   def index
-    @report = FinancialControl.last
-    @total_revenue = @report.revenues.sum { |rev| rev.amount }
-    @total_expense = @report.expenses.sum { |exp| exp.amount }
+    report = FinancialControl.last
+    @revenues = report.financial_movements.where(movement_type: :revenue)
+    @expenses = report.financial_movements.where(movement_type: :expense)
+    @total_revenue = @revenues.sum { |revenue| revenue.amount }
+    @total_expense = @expenses.sum { |expense| expense.amount }
     @balance = @total_revenue - @total_expense
   end
 end
